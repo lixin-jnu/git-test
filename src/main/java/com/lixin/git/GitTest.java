9b1a06ec3d5608e2c9f9be1d18a606ec2a197741
lixin <lixinseu@163.com>
2022-03-11 18:58:14 +0800

    ht-1

--- src/main/java/com/lixin/git/GitTest.java
+++ src/main/java/com/lixin/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("v1");
         System.out.println("v2");
+        System.out.println("ht-1");
     }
 
 }
